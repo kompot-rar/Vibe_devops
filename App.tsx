229e0155f24d69281e4dd5c1d9896b0ba12fed1f
Łukasz Mróz <kompot92@gmail.com>
2026-01-06 12:16:26 +0100

    Change router from HashRouter to BrowserRouter

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import BlogList from './components/BlogList';
 import Roadmap from './components/Roadmap';
